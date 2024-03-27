28166c4d921be4fddf702faacd3174127b06f02d
YuriusuDev <yuriusu.dev@gmail.com>
2024-03-27 21:02:23 +0800

    2024.03.27-21:02:23

--- esbuild.js
+++ esbuild.js
@@ -1,6 +1,15 @@
 import {context} from "esbuild";
 
 const esbuild = await context({
+  define: {
+    "FIREBASE_API_KEY": "''",
+    "FIREBASE_AUTH_DOMAIN": "''",
+    "FIREBASE_PROJECT_ID": "''",
+    "FIREBASE_STORAGE_BUCKET": "''",
+    "FIREBASE_MESSAGING_SENDER_ID": "''",
+    "FIREBASE_APP_ID": "''",
+    "FIREBASE_MEASUREMENT_ID": "''"
+  },
   entryPoints: [
     "./src/*"
   ],
@@ -27,16 +36,7 @@ const esbuild = await context({
   minifyIdentifiers: true,
   minifyWhitespace: true,
   minifySyntax: true,
-  write: true,
-  define: {
-    "process.env.FIREBASE_API_KEY": "'AIzaSyDwtVZ_a_CwxM4GrRKFl04hDTNwzPMPcFg'",
-    "process.env.FIREBASE_AUTH_DOMAIN": "'yuriusu-superchat.firebaseapp.com'",
-    "process.env.FIREBASE_PROJECT_ID": "'yuriusu-superchat'",
-    "process.env.FIREBASE_STORAGE_BUCKET": "'yuriusu-superchat.appspot.com'",
-    "process.env.FIREBASE_MESSAGING_SENDER_ID": "'58217748697'",
-    "process.env.FIREBASE_APP_ID": "'1:58217748697:web:c7896177f3075392e246c8'",
-    "process.env.FIREBASE_MEASUREMENT_ID": "'G-CE5H8W2SR9'"
-  }
+  write: true
 });
 
 await esbuild.watch();
